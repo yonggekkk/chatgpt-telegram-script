c4f6961eb409d95cb3869cb3229c21105df7a8b7
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2023-02-15 15:31:37 +0800

    Update chatgpt.sh

--- chatgpt.sh
+++ chatgpt.sh
@@ -33,9 +33,39 @@ red "不支持你当前系统，请选择使用Ubuntu,Debian,Centos系统。" &&
 fi
 
 [[ $(type -P yum) ]] && yumapt='yum -y' || yumapt='apt -y'
-[[ ! $(type -P python3) ]] && (yellow "检测到python3未安装，升级安装中" && $yumapt update;$yumapt install python3)
+if [[ $release = Centos ]]; then
+if [[ ${vsid} =~ 8 ]]; then
+cd /etc/yum.repos.d/ && mkdir backup && mv *repo backup/ 
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
+sed -i -e "s|mirrors.cloud.aliyuncs.com|mirrors.aliyun.com|g " /etc/yum.repos.d/CentOS-*
+sed -i -e "s|releasever|releasever-stream|g" /etc/yum.repos.d/CentOS-*
+yum clean all && yum makecache
+fi
+yum install epel-release -y
+[[ ! $(type -P python3-devel) ]] && ($yumapt update;$yumapt install python3-devel python3 -y)
+else
+$yumapt update
+[[ ! $(type -P python3-pip) ]] && ($yumapt update;$yumapt install python3-pip -y)
+fi
 
-apt update && pip install -U pip && pip install openai aiogram 
+py3=`python3 -V  | awk '{print $2}' | tr -d '.'`
+if [[ $py3 -le 370 ]]; then
+yellow "检测到python3版本小于3.7.0，现在升级到3.7.3，升级时间比较长，请稍等……"
+wget -N https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+tar -zxf Python-3.7.3.tgz
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc libffi-devel make -y
+cd Python-3.7.3
+./configure --prefix=/usr/local/python3.7
+make && make install
+co=$(echo $? 2>&1)
+if [[ $co = 0 ]]; then
+green "升级python3成功"
+ln -sf /usr/local/python3.7/bin/python3.7 /usr/bin/python3
+else
+red "升级python3失败" 
+fi
+fi
+pip3 install -U pip && pip3 install openai aiogram 
 cat > TGchatgpt.py << EOF
 import openai
 from aiogram import Bot, types
@@ -54,7 +84,7 @@ async def send(message : types.Message):
     max_tokens=1000,
     top_p=1.0,
     frequency_penalty=0.0,
-    presence_penalty=0.6,
+    presence_penalty=0.0,
     stop=["You:"]
 )
     await message.answer(response['choices'][0]['text'])
@@ -80,7 +110,6 @@ EOF
 systemctl daemon-reload
 systemctl enable Chatgpt.service
 systemctl start Chatgpt.service
-systemctl restart Chatgpt.service
 
 green "Chatgpt Telegram机器人安装完毕"
 
